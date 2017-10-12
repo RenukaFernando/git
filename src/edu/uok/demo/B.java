b43ba5f6091cdc25c7efa06401a2497edb1621f0
Renuka Fernando <renukapiyumal@gmail.com>
2017-10-12 14:59:17 +0530

    add class B

---
+++ src/edu/uok/demo/B.java
@@ -0,0 +1,11 @@
+package edu.uok.demo;
+
+public class B {
+    private int age;
+    private String name;
+
+    public B(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
