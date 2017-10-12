1136fabda51f5af750233cce968376fb8556da41
Renuka Fernando <renukapiyumal@gmail.com>
2017-10-12 14:56:24 +0530

    add for loop

--- src/edu/uok/demo/A.java
+++ src/edu/uok/demo/A.java
@@ -1,5 +1,10 @@
 package edu.uok.demo;
 
 public class A {
+    private int x;
+    private int y;
 
+    A(){
+        this.x = 1000;
+    }
 }
--- src/edu/uok/demo/Main.java
+++ src/edu/uok/demo/Main.java
@@ -3,5 +3,10 @@ package edu.uok.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World");
+        A a = new A();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
